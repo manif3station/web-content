530548997b81e40076e12bfde185f4d82ad29e11
Michael Vu <email@michael.vu>
2022-11-28 15:02:19 +0000

    fisrt commit

---
+++ src/lib/Web/Content.pm
@@ -0,0 +1,81 @@
+package Web::Content;
+
+use Moo;
+use JSON::XS ();
+use YAML::XS ();
+use Template;
+
+if (!$ENV{WEB_CONTENT_BASE_DIR}) {
+    warn ">> You can set where the content base dir is 'WEB_CONTENT_BASE_DIR'";
+}
+
+has dir => ( is => 'ro', lazy => 1, builder => 1 );
+
+sub _build_dir { $ENV{WEB_CONTENT_BASE_DIR} // "/app/src/content" }
+
+has json => ( is => 'ro', lazy => 1, builder => 1 );
+
+sub _build_json { JSON::XS->new->canonical->pretty->allow_nonref->utf8 }
+
+sub _content {
+    my ( $self, $file, $encoding ) = @_;
+    $encoding //= 'utf8';
+    open my $fh, '<' . $encoding, $file;
+    local $/;
+    <$fh>;
+}
+
+sub get {
+    my ( $self, $path ) = @_;
+
+    $path = [ split /\./, $path ];
+
+    my $data = $self->get_data_from_file( $path, $self->dir );
+
+    return $data if !@$path || !$data || !ref $data;
+
+    my $remain_path = join '.', @$path;
+
+    Template->new->process(
+        \"[%store(data.$remain_path)%]" => {
+            data  => $data,
+            store => sub { ($data) = @_; undef },
+        },
+        \my $undef,
+    );
+
+    return $data;
+}
+
+sub get_data_from_file {
+    my ( $self, $path, $location ) = @_;
+
+    while ( my $item = shift @$path ) {
+        $location .= "/$item";
+
+        if ( -d $location ) {
+            next;
+        }
+        elsif ( -f "$location.json" ) {
+            return $self->json->decode( $self->_content("$location.json") );
+        }
+        elsif ( -f "$location.yml" ) {
+            return YAML::XS::Load( $self->_content("$location.yml") );
+        }
+        elsif ( -f "$location.yaml" ) {
+            return YAML::XS::Load( $self->_content("$location.yaml") );
+        }
+        elsif ( -f "$location.txt" ) {
+            return $self->_content("$location.txt");
+        }
+        elsif ( -f $location ) {
+            return $self->_content( $location, 'raw' );
+        }
+        else {
+            return undef;
+        }
+    }
+    return undef;
+}
+
+1;
---
+++ t/content.t
@@ -0,0 +1,69 @@
+use strict;
+use warnings;
+
+use File::Path 'mkpath';
+use Test::More;
+use Web::Content;
+
+my $dir = 't/samples/foo/bar/zxy';
+
+mkpath $dir;
+
+my $content = Web::Content->new( dir => 't/samples' );
+
+subtest "get from json file" => sub {
+    open my $fh, '>', "$dir/data1.json";
+    print $fh '{"a": {"b": {"c": "here"}}}';
+    close $fh;
+    my $path = 'foo.bar.zxy.data1.a.b.c';
+    is $content->get($path), 'here';
+};
+
+subtest "get from yaml file" => sub {
+    open my $fh, '>', "$dir/data2.yml";
+    print $fh <<'HEREDOC';
+a:
+    b:
+        c: there
+HEREDOC
+    close $fh;
+    my $path = 'foo.bar.zxy.data2.a.b.c';
+    is $content->get($path), 'there';
+};
+
+subtest "get from text file" => sub {
+    open my $fh, '>', "$dir/data3.txt";
+    print $fh 'here and there';
+    close $fh;
+    my $path = 'foo.bar.zxy.data3.a.b.c';
+    is $content->get($path), 'here and there';
+};
+
+subtest 'get from json again' => sub {
+    open my $fh, '>', "t/samples/foo1.json";
+    print $fh '{"bar":{"zxy":{"data":{"a":{"b":{"c":"good"}}}}}}';
+    close $fh;
+    my $path = 'foo1.bar.zxy.data.a.b.c';
+    is $content->get($path), 'good';
+};
+
+subtest 'get from json again' => sub {
+    open my $fh, '>', "t/samples/foo2.yml";
+    print $fh <<'HEREDOC';
+bar:
+    zxy:
+        data:
+            a:
+                b:
+                    c: bad
+HEREDOC
+    close $fh;
+    my $path = 'foo2.bar.zxy.data.a.b.c';
+    is $content->get($path), 'bad';
+};
+
+subtest "not found" => sub {
+    is $content->get('foo.bar.somewhere'), undef;
+};
+
+done_testing;
